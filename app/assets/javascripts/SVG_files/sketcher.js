f7812e1ef056f1bbe7a3db9f03b87f4d816b82bc
jrflood <jrflood@illinois.edu>
2015-10-29 14:30:02 -0500

    Introduce wheel scroll zoom.  Need to zoom to cursor position as well.

--- app/assets/javascripts/SVG_files/sketcher.js
+++ app/assets/javascripts/SVG_files/sketcher.js
@@ -18,20 +18,25 @@ function Sketcher( canvasID, brushImage ) {
     this.mouseDownEvent = "mousedown";
     this.mouseMoveEvent = "mousemove";
     this.mouseUpEvent = "mouseup";
-    this.mouseWheelEvent = "handleScroll";
+
+    this.canvas.bind('DOMMouseScroll mousewheel', function(e)     // inline function vs cutout to prototype
+    {
+      e.stopImmediatePropagation();
+      e.stopPropagation();
+      var deltaDiv = 1000;                              // default for non-FireFox
+     if (e.type == "DOMMouseScroll") {deltaDiv = 100}   // adjust for FireFox
+      var delta = parseInt(e.originalEvent.wheelDelta || -e.originalEvent.detail);
+      zoom += delta / deltaDiv;
+      var zoomX = e.originalEvent.clientX - this.offsetLeft;
+      var zoomY = e.originalEvent.clientY - this.offsetTop;
+      //zoom_trans(zoomX, zoomY, zoom);
+      zoom_trans(null, null, zoom);
+      return e.preventDefault() && false;
+    });
   }
 
   this.canvas.bind( this.mouseDownEvent, this.onCanvasMouseDown() );
 }
-Sketcher.prototype.handleScroll = function(e)
-{
-  var delta = e.wheelDelta ? e.wheelDelta/100 : e.detail ? -e.detail/6 : 0;
-  if (delta)
-    zoom(null, null, delta);
-  return e.preventDefault() && false;
-};
-//this.canvas.addEventListener('DOMMouseScroll',handleScroll,false);  // not sure I want scroll
-//this.canvas.addEventListener('mousewheel',handleScroll,false);
 
 Sketcher.prototype.onCanvasMouseDown = function () {
   var self = this;
@@ -85,7 +90,7 @@ Sketcher.prototype.onCanvasMouseUp = function (event) {
     self.mouseMoveHandler = null;
     self.mouseUpHandler = null;
     //self.context.restore();
-    if (cursorMode == "DRAW") {
+    if (cursorMode == "PATH") {
       if (thisSvg != undefined) {
         if (thisSvg.length > 0) {
         svg.push(thisSvg);    // save this graphic item for replot
@@ -95,7 +100,7 @@ Sketcher.prototype.onCanvasMouseUp = function (event) {
             element = document.createElementNS('http://www.w3.org/2000/svg', 'line');
             element.setAttributeNS(null, 'stroke', 'red');
             element.setAttributeNS(null, 'stroke-width', '3');
-            element.setAttributeNS(null, 'stroke-opacity', '0.5');
+            element.setAttributeNS(null, 'stroke-opacity', '1.0');
             element.setAttributeNS(null, 'stroke-linecap', 'round');
             //svgLayer.appendChild(element);
             document.getElementById("xlt").appendChild(element);
@@ -130,35 +135,36 @@ Sketcher.prototype.updateMousePosition = function (event) {
 };
 
 Sketcher.prototype.updateCanvasByLine = function (event) {
-  if(cursorMode == "DRAW") {          // modified for move/draw mode JRF
+  if(cursorMode != "MOVE") {          // modified for move/draw mode JRF
     ////////////////////
     //
     //  DRAW mode needs to be expanded to Circle, Rectangle, Text, Line, PolyLine, Marker(?)
     //  mouseDown/Up will need to articulate for above shapes also
     //
     ////////////////////
-    if (drawMode == "PATH") {
+    if (cursorMode == "PATH") {
       this.context.beginPath();        // interdigitate canvas draw and record svg
       var thisX = this.lastMousePoint.x;
       var thisY = this.lastMousePoint.y;
       this.context.moveTo(thisX, thisY);
-      //thisSvg += '<line stroke="red" x1="' + this.lastMousePoint.x + '" y1="' + this.lastMousePoint.y + '" ';
-      thisSvg.push([(thisX - xC) / Math.pow(2, zoom), (thisY - yC) / Math.pow(2, zoom)]);
+//  this still needs scaling to original image extent
+      thisSvg.push([(thisX - xC) / zoom, (thisY - yC) / zoom]);
       this.updateMousePosition(event);
       var thisX = this.lastMousePoint.x;
       var thisY = this.lastMousePoint.y;
       this.context.lineTo(thisX, thisY);
-      //thisSvg += 'x2="' + this.lastMousePoint.x + '" y2="' + this.lastMousePoint.y + '">\n';
-      thisSvg.push([(thisX - xC) / Math.pow(2, zoom), (thisY - yC) / Math.pow(2, zoom)]);
+      thisSvg.push([(thisX - xC) / zoom, (thisY - yC) / zoom]);
       this.context.strokeStyle = "#0066FF";
       this.context.stroke();
+    } else if (cursorMode == "POLYGON") {
+      //need aditional articulation of mouseDown/Up/Click to begin/end each segment of polygon
+    } else if (cursorMode == "CIRCLE") {
+
+    } else if (cursorMode == "LINE") {
+
+    } else if (cursorMode == "TEXT") {
+
     }
-  } else if (drawMode == "POLYGON") {
-    //need aditional articulation of mouseDown/Up/Click to begin/end each segment of polygon
-  } else if (drawMode == "CIRCLE") {
-
-  } else if (drawMode == "LINE") {
-
   }
   //else {    // this version depends on xC, yC resampling the image to re-origin the image
   //  var oldX = this.lastMousePoint.x;   //latch the x and y
@@ -171,7 +177,7 @@ Sketcher.prototype.updateCanvasByLine = function (event) {
   //  //this.context.translate(-(oldX - this.lastMousePoint.x), -(oldY - this.lastMousePoint.y));
   //  renderImage();
   //}
-  else {    // this version assumes manipulating the left and top attributes of the canvas
+  else {    // this version assumes manipulating the left and top attributes of the canvas (?)
     var oldX = this.lastMousePoint.x;
     var oldY = this.lastMousePoint.y;
     this.updateMousePosition(event);
@@ -179,8 +185,6 @@ Sketcher.prototype.updateCanvasByLine = function (event) {
     yC = yC - (oldY - this.lastMousePoint.y);
     var newX = this.lastMousePoint.x;
     var newY = this.lastMousePoint.y;
-    //xC = newX;
-    //yC = newY;
     if (oldX == newX && oldY == newY) return false;
     //var style = $("#svgLayer")[0].style;
     //style.left = (style.left.split('px')[0] - (oldX - newX)).toString() + 'px';
@@ -188,7 +192,8 @@ Sketcher.prototype.updateCanvasByLine = function (event) {
     //style = $("#sketch")[0].style;
     //style.left = (-xC /*+ parseInt(style.left.split('px')[0]) - (newX - oldX)*/).toString() + 'px';
     //style.top = (-yC /*+ parseInt(style.top.split('px')[0]) + (newY - oldY)*/).toString() + 'px';
-    zoom_trans(xC, yC, zoom);
+    //zoom_trans(xC, yC, zoom);
+    zoom_trans(null, null, zoom);
     //this.context.translate(-(oldX - this.lastMousePoint.x), -(oldY - this.lastMousePoint.y));
     //renderImage();
     u = 0;
@@ -217,20 +222,20 @@ Sketcher.prototype.updateCanvasByBrush = function (event) {
   }
 };
 
-Sketcher.prototype.toString = function () {
-
-  var dataString = this.canvas.get(0).toDataURL("image/png");
-  var index = dataString.indexOf( "," )+1;
-  dataString = dataString.substring( index );
-
-  return dataString;
-};
-
-Sketcher.prototype.toDataURL = function () {
-
-  var dataString = this.canvas.get(0).toDataURL("image/png");
-  return dataString;
-};
+//Sketcher.prototype.toString = function () {
+//
+//  var dataString = this.canvas.get(0).toDataURL("image/png");
+//  var index = dataString.indexOf( "," )+1;
+//  dataString = dataString.substring( index );
+//
+//  return dataString;
+//};
+//
+//Sketcher.prototype.toDataURL = function () {
+//
+//  var dataString = this.canvas.get(0).toDataURL("image/png");
+//  return dataString;
+//};
 
 Sketcher.prototype.clear = function () {
 
